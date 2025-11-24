8c1591d4327ed20df6902e5a2d1bea810f4719a7
asp <asp@Aptechnk1.edu>
2025-11-24 18:53:27 +0500

    all

---
+++ frontend/src/components/Navbarr.js
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Navbarr() {
+  return (
+    <Navbar expand="lg" bg="light" className="shadow-sm">
+      <Container>
+        <Navbar.Brand href="/">MyWebsite</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/register">Register</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navbarr;
